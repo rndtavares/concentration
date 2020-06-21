6213338ec076006e44fdbf502363539d84ae0894
Ronaldo Tavares da Silva <rtsilva@ci-63310-6.uolcorp.intranet>
2020-06-21 12:21:06 -0300

    first commit

---
+++ Concentration.swift
@@ -0,0 +1,55 @@
+//
+//  Concentration.swift
+//
+//  Created by CS193p Instructor  on 09/25/17.
+//  Copyright © 2017 Stanford University. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    
+    var cards = [Card]()
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil    // not one and only ...
+            } else {
+                // either no card or two cards face up
+                for flipdownIndex in cards.indices {
+                    cards[flipdownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+            
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        startGame(numberOfPairsOfCards: numberOfPairsOfCards)
+    }
+    
+    func startGame(numberOfPairsOfCards: Int){
+        cards.removeAll()
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        //    TODO: Shuffle the cards
+        var shuflledCards = [Card]()
+        while cards.count > 1 {
+            let randomIndex = Int(arc4random_uniform(UInt32(cards.count - 1)))
+            shuflledCards.append(cards.remove(at: randomIndex))
+        }
+        cards += shuflledCards
+    }
+}
