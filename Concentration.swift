323484f3632760cc1a076fd79cc7bd7c388ea154
Ronaldo Tavares da Silva <rndtavares@gmail.com>
2020-06-24 10:40:24 -0300

    structing concentration

--- Concentration.swift
+++ Concentration.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class Concentration {
+struct Concentration {
     
     private(set) var cards = [Card]()
     private var indexOfOneAndOnlyFaceUpCard: Int? {
@@ -31,7 +31,7 @@ class Concentration {
         }
     }
     
-    func chooseCard(at index: Int) {
+    mutating func chooseCard(at index: Int) {
         assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chosen index not in the cards")
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
@@ -52,7 +52,7 @@ class Concentration {
         startGame(numberOfPairsOfCards: numberOfPairsOfCards)
     }
     
-    func startGame(numberOfPairsOfCards: Int){
+    mutating func startGame(numberOfPairsOfCards: Int){
         assert(numberOfPairsOfCards > 0, "Concentration.startGame(\(numberOfPairsOfCards)): you must have at least one pair of cards")
         cards.removeAll()
         for _ in 1...numberOfPairsOfCards {
