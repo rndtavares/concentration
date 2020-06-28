dc381670a1ab7d76ad241505f340f90f7c607e76
Ronaldo Tavares da Silva <rndtavares@gmail.com>
2020-06-28 12:57:48 -0300

    Refactor Closures and others

--- Concentration.swift
+++ Concentration.swift
@@ -12,17 +12,7 @@ struct Concentration {
     private(set) var cards = [Card]()
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
         }
         set {
             for index in cards.indices {
@@ -65,5 +55,11 @@ struct Concentration {
             shuflledCards.append(cards.remove(at: cards.count.arc4random))
         }
         cards += shuflledCards
+    }
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
--- ViewController.swift
+++ ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-	
+	 
     private lazy var game: Concentration =
         Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     
@@ -18,11 +18,24 @@ class ViewController: UIViewController {
 	
 	private(set) var flipCount = 0 {
 		didSet {
-			flipCountLabel.text = "Flips: \(flipCount)"
+            updateFlipCountLabel()
 		}
 	}
+    
+    private func updateFlipCountLabel(){
+        let attributes: [NSAttributedStringKey:Any] = [
+            .strokeWidth : 5.0,
+            .strokeColor : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        ]
+        let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+        flipCountLabel.attributedText = attributedString
+    }
 	
-	@IBOutlet private weak var flipCountLabel: UILabel!
+    @IBOutlet private weak var flipCountLabel: UILabel! {
+        didSet {
+            updateFlipCountLabel()
+        }
+    }
 	
 	@IBOutlet private var cardButtons: [UIButton]!
 	
@@ -52,13 +65,14 @@ class ViewController: UIViewController {
 		
 	}
 	
-	private var emojiChoices = ["🦇", "😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"]
+	private var emojiChoices = "🦇😱🙀😈🎃👻🍭🍬🍎"
 	
 	private var emoji = [Card: String]()
 	
 	private func emoji(for card: Card) -> String {
 		if emoji[card] == nil, emojiChoices.count > 0 {
-			emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+			emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
 		}
 		return emoji[card] ?? "?"
 	}
@@ -66,7 +80,7 @@ class ViewController: UIViewController {
     @IBAction func newGame(_ sender: Any) {
         game.startGame(numberOfPairsOfCards: numberOfPairsOfCards)
         flipCount = 0
-        emojiChoices = ["🦇", "😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"]
+        emojiChoices = "🦇😱🙀😈🎃👻🍭🍬🍎"
         updateViewFromModel()
     }
     
