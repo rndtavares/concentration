1e502504072f16b98896a2c7b62324bb6c63ecd7
Ronaldo Tavares da Silva <rndtavares@gmail.com>
2020-08-26 10:48:44 -0300

    adding controller theme choice

---
+++ ConcentrationThemeChooserViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ConcentrationThemeChooserViewController.swift
+//  Lecture 1 - Concentration
+//
+//  Created by Ronaldo Tavares da Silva on 25/08/2020.
+//  Copyright © 2020 Michel Deiman. All rights reserved.
+//
+
+import UIKit
+
+class ConcentrationThemeChooserViewController: UIViewController {
+
+    let gameThemeArray = ["Halloween":"🦇😱🙀😈🎃👻🍭🍬🍎",
+                                  "Emojis":"😀☺️😍😭🤓😔😡😱🤯🤭😴",
+                                  "Animals":"🐶🐱🐭🦊🐼🐨🦁🐮🐷🐸🐵",
+                                  "Sports":"⚽️🏀🏈⚾️🎾🏐🏉🎱🏆🥇",
+                                  "Vehicles":"🚗🚔🚍🚲🛴🚒🚁✈️🛳🚊",
+                                  "Flags":"🇦🇷🇧🇷🇨🇦🇯🇵🇿🇦🇩🇪🇺🇸🇪🇸🇬🇷🇮🇱"]
+    
+    let gameColorArray = ["Halloween" : [#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1),#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)],
+                                  "Emojis" : [#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1),#colorLiteral(red: 0.5058823824, green: 0.3372549117, blue: 0.06666667014, alpha: 1)],
+                                  "Animals" : [#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)],
+                                  "Sports" : [#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)],
+                                  "Vehicles" : [#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1),#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)],
+                                  "Flags" : [#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1),#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)]]
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Choose Theme" {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = gameThemeArray[themeName], let colors = gameColorArray[themeName] {
+                if let cvc = segue.destination as? ConcentrationViewController {
+                    cvc.actualColorThemes = colors
+                    cvc.theme = theme
+                }
+            }
+        }
+    }
+}
--- ConcentrationViewController.swift
+++ ConcentrationViewController.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConcentrationViewController: UIViewController {
 	 
     private lazy var game: Concentration =
         Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     
     var numberOfPairsOfCards: Int {
+        guard cardButtons != nil else { return 0 }
         return (cardButtons.count + 1) / 2
     }
 	 
@@ -64,38 +65,41 @@ class ViewController: UIViewController {
 	}
 	
 	func updateViewFromModel() {
+        guard cardButtons != nil else { return }
         updateFlipCountLabel()
         updateScoreLabel()
-		for index in cardButtons.indices {
-			let button = cardButtons[index]
-			let card = game.cards[index]
-			if card.isFaceUp {
-				button.setTitle(emoji(for: card), for: UIControlState.normal)
-				button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-			} else {
-				button.setTitle("", for: UIControlState.normal)
-				button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : actualColorThemes[0]
-			}
-		}
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            } else {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : actualColorThemes[0]
+            }
+        }
         concentrationView.backgroundColor = actualColorThemes[1]
 	}
-	
-    private var gameThemeArray = ["🦇😱🙀😈🎃👻🍭🍬🍎" : [#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1),#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)],
-                                     "😀☺️😍😭🤓😔😡😱🤯🤭😴" : [#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1),#colorLiteral(red: 0.5058823824, green: 0.3372549117, blue: 0.06666667014, alpha: 1)],
-                                     "🐶🐱🐭🦊🐼🐨🦁🐮🐷🐸🐵" : [#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)],
-                                     "⚽️🏀🏈⚾️🎾🏐🏉🎱🏆🥇" : [#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)],
-                                     "🚗🚔🚍🚲🛴🚒🚁✈️🛳🚊" : [#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1),#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)],
-                                     "🇦🇷🇧🇷🇨🇦🇯🇵🇿🇦🇩🇪🇺🇸🇪🇸🇬🇷🇮🇱" : [#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1),#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)]]
+    
+    var theme: String? {
+        didSet {
+            actualGameEmojiChoices = theme ?? ""
+            emoji = [:]
+            updateViewFromModel()
+        }
+    }
+    
     private var seenCards = ""
     private var actualGameEmojiChoices = ""
-    private var actualColorThemes = [#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1),#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)]
+    var actualColorThemes = [#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1),#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)]
 	
 	private var emoji = [Card: String]()
 	
 	private func emoji(for card: Card) -> String {
-        if actualGameEmojiChoices == "" {
-            selectTheme()
-        }
+//        if actualGameEmojiChoices == "" {
+//            selectTheme()
+//        }
 		if emoji[card] == nil, actualGameEmojiChoices.count > 0 {
             let randomStringIndex = actualGameEmojiChoices.index(actualGameEmojiChoices.startIndex, offsetBy: actualGameEmojiChoices.count.arc4random)
 			emoji[card] = String(actualGameEmojiChoices.remove(at: randomStringIndex))
@@ -103,15 +107,15 @@ class ViewController: UIViewController {
 		return emoji[card] ?? "?"
 	}
     
-    private func selectTheme(){
-        let randomStringIndex = gameThemeArray.index(gameThemeArray.startIndex, offsetBy: gameThemeArray.count.arc4random)
-        actualGameEmojiChoices = gameThemeArray[randomStringIndex].key
-        actualColorThemes = gameThemeArray[randomStringIndex].value
-    }
+//    private func selectTheme(){
+//        let randomStringIndex = gameThemeArray.index(gameThemeArray.startIndex, offsetBy: gameThemeArray.count.arc4random)
+//        actualGameEmojiChoices = gameThemeArray[randomStringIndex].key
+//        actualColorThemes = gameThemeArray[randomStringIndex].value
+//    }
     
     @IBAction func newGame(_ sender: Any) {
         game.startGame(numberOfPairsOfCards: numberOfPairsOfCards)
-        selectTheme()
+//        selectTheme()
         updateViewFromModel()
     }
     
@@ -128,18 +132,3 @@ extension Int {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
